238784bfcadd0429b89455aaef1a88832297915f
larexx40 <larexx40@gmail.com>
2024-01-19 08:07:12 +0100

    done with role model

---
+++ src/Models/role.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface Role extends Document {
+  name: string;
+  domain: {
+    name: string;
+    permissions: string[];
+  };
+}
+
+const roleSchema = new Schema<Role>({
+  name: { type: String, required: true, unique: true },
+  domain: {
+    name: { type: String, required: true },
+    permissions: [{ type: String, ref: 'Permission' }], // Reference to the Permission model
+  },
+});
+
+const RoleModel = mongoose.model<Role>('Role', roleSchema);
+
+export default RoleModel;
