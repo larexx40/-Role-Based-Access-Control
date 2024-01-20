ea23b1a73da4e2b72f78e4cc296b4346fd29bf1b
larexx40 <larexx40@gmail.com>
2024-01-20 09:07:27 +0100

    done with signup

--- src/Controllers/users.ts
+++ src/Controllers/users.ts
@@ -1,7 +1,9 @@
-import { UserData } from './../Types/types';
+import { AuthTokenPayload, UserData } from './../Types/types';
 import { NextFunction, Request, Response } from "express";
 import { OTPExpiryTime, generateVerificationOTP, hashPassword } from "../Utils/utils";
 import UserRepository from "../Repositories/users";
+import { signJwt } from '../Utils/jwt';
+import mongoose from 'mongoose';
 
 class UserController{
     static async signup(req: Request, res: Response, next: NextFunction): Promise<void>{
@@ -27,62 +29,100 @@ class UserController{
             return;
         }
 
-        //check if unique data exist
-        const [isEmailExist, isPhonenoExist, isUsernameExist] = await Promise.all([
-            UserRepository.checkIfExist({email}), 
-            UserRepository.checkIfExist({phoneno}), 
-            username ? UserRepository.checkIfExist({username}): null
-        ])
+        try {
+            //check if unique data exist
+            const [isEmailExist, isPhonenoExist, isUsernameExist] = await Promise.all([
+                UserRepository.checkIfExist({email}), 
+                UserRepository.checkIfExist({phoneno}), 
+                username ? UserRepository.checkIfExist({username}): null
+            ])
 
-        if(isEmailExist){
-            res.status(400).json({ 
+            if(isEmailExist){
+                res.status(400).json({ 
+                    status: false, 
+                    message: "Account with email already exist",
+                    error: [],
+                    data: []
+                });
+                return;
+            }
+
+            if(isPhonenoExist){
+                res.status(400).json({ 
+                    status: false, 
+                    message: "Account with phone number already exist",
+                    error: [],
+                    data: []
+                });
+                return;
+            }
+
+            if(isUsernameExist){
+                res.status(400).json({ 
+                    status: false, 
+                    message: "Account with username already exist",
+                    error: [],
+                    data: []
+                });
+                return;
+            }
+
+            const newPassword = await hashPassword(password);
+            const verificationOtp = generateVerificationOTP();
+            const verificationOtpExpiry = new Date(Date.now() + OTPExpiryTime)
+            const userData: UserData = {
+                name,
+                email,
+                phoneno, 
+                password: newPassword,
+                roles: role, 
+                username, 
+                dob, 
+                address,
+                verificationOtp,
+                verificationOtpExpiry
+            }
+
+            //save user 
+            const saveUser = await UserRepository.createUser(userData);
+            const jwtPayload: AuthTokenPayload ={
+                userid: saveUser._id,
+                email: email,
+                roles: role 
+            } 
+            const jwtToken = signJwt(jwtPayload)
+            //send welcome mail
+            //send verification otp mail
+
+            res.status(201).json({
+                status: true,
+                message: "User registration successful",
+                data: saveUser,
+                authToken: jwtToken,
+            });
+        } catch (error) {
+            if (error instanceof mongoose.Error.ValidationError) {
+                // Mongoose validation error
+                const validationErrors = Object.values(error.errors).map((err) => err.message);
+                res.status(400).json({ 
+                    status: false,
+                    message: "Invalid data passed",
+                    error: validationErrors,
+                    data:[]
+                });
+            }
+            res.status(500).json({ 
                 status: false, 
-                message: "Account with email already exist",
+                message: "Internal server error",
                 error: [],
                 data: []
             });
-            return;
+            console.error(error);
         }
 
-        if(isPhonenoExist){
-            res.status(400).json({ 
-                status: false, 
-                message: "Account with phone number already exist",
-                error: [],
-                data: []
-            });
-            return;
-        }
-
-        if(isUsernameExist){
-            res.status(400).json({ 
-                status: false, 
-                message: "Account with username already exist",
-                error: [],
-                data: []
-            });
-            return;
-        }
-
-        const newPassword = await hashPassword(password);
-        const verificationOtp = generateVerificationOTP();
-        const verificationOtpExpiry = new Date(Date.now() + OTPExpiryTime)
-        const userData: UserData = {
-            name,
-            email,
-            phoneno, 
-            password: newPassword,
-            roles: role, 
-            username, 
-            dob, 
-            address,
-            verificationOtp,
-            verificationOtpExpiry
-        }
-
-        //save user 
-        const save = await UserRepository.createUser(userData);
     }
+
+    
 }
 
 export default UserController;
