4413a533954df955de4a7cbc8c341b8b604a37bb
larexx40 <larexx40@gmail.com>
2024-01-22 06:50:21 +0100

    setup user route

--- src/Routes/users.ts
+++ src/Routes/users.ts
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import UserController from "../Controllers/users";
 const userRouter = Router();
+
+userRouter.post('/signup', UserController.signup);
+userRouter.post('/verify-email', UserController.verifyMail)
 
 export default userRouter;
